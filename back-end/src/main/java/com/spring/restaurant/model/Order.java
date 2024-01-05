89f2feed91755711d1f893cdc918623501dd33e5
azzaantar95 <azzaantar95@gmail.com>
2024-01-05 11:44:35 +0200

    create category and order model

---
+++ back-end/src/main/java/com/spring/restaurant/model/Order.java
@@ -0,0 +1,11 @@
+package com.spring.restaurant.model;
+
+public class Order {
+
+    private Long id;
+    private String orderName;
+    private int price;
+    private String img;
+    private String description;
+
+}
