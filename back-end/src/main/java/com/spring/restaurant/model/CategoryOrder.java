a17ec811a55460787fccb4355083b3959327af71
azzaantar95 <azzaantar95@gmail.com>
2024-01-07 13:48:35 +0200

    creation timestamp and update timestamp

--- back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -7,6 +7,11 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -14,6 +19,14 @@ import lombok.NoArgsConstructor;
 @MappedSuperclass
 public class CategoryOrder extends BaseEntity {
 
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "dateCreation")
+    @CreationTimestamp
+    private Date dateCreation;
+
+    @Column(name = "dateUpdate")
+    @UpdateTimestamp
+    private Date dateUpdate;
 }
