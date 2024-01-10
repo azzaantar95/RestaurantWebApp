279449087f9423d03e45bfcf804566169e805f66
azzaantar95 <azzaantar95@gmail.com>
2024-01-10 03:21:05 +0200

    getAllOrders

--- back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
+++ back-end/src/main/java/com/spring/restaurant/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.spring.restaurant.controller;
 
+import com.spring.restaurant.model.Order;
 import com.spring.restaurant.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -12,5 +16,10 @@ public class OrderController {
     @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @GetMapping("api/allOrders")
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
     }
 }
--- back-end/src/main/java/com/spring/restaurant/service/OrderService.java
+++ back-end/src/main/java/com/spring/restaurant/service/OrderService.java
@@ -1,15 +1,23 @@
 package com.spring.restaurant.service;
 
 import com.spring.restaurant.dao.OrderRepo;
+import com.spring.restaurant.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
     private OrderRepo orderRepo;
-    @Autowired
 
+    @Autowired
     public OrderService(OrderRepo orderRepo) {
         this.orderRepo = orderRepo;
+    }
+
+    public List<Order> getAllOrders() {
+
+        return orderRepo.findAll();
     }
 }
